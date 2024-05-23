d1c6b1c8be07366b79c29f08cad3d2ccf1e4d124
Lorwik <manueljsandalio@gmail.com>
2024-05-23 14:12:36 +0200

    Termino el renderizado de cabezas.

--- src/main/java/org/nexus/indexador/controllers/frmCabezas.java
+++ src/main/java/org/nexus/indexador/controllers/frmCabezas.java
@@ -6,10 +6,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import org.nexus.indexador.models.grhData;
 import org.nexus.indexador.models.headData;
+import org.nexus.indexador.utils.configManager;
 
+import java.io.File;
 import java.io.IOException;
 
 public class frmCabezas {
@@ -36,11 +41,17 @@ public class frmCabezas {
     private headData headDataManager; // Objeto que gestiona los datos de las cabezas, incluyendo la carga y manipulación de los mismos
     private ObservableList<headData> headList; // Lista observable que contiene los datos de los gráficos indexados.
 
+    private org.nexus.indexador.utils.configManager configManager; // Objeto encargado de manejar la configuración de la aplicación, incluyendo la lectura y escritura de archivos de configuración.
+
     @FXML
     protected void initialize() {
+
+        configManager = org.nexus.indexador.utils.configManager.getInstance();
+
         headDataManager = new headData(); // Crear una instancia de grhData
 
         loadHeadData();
+        setupHeadListListener();
     }
 
     private void loadHeadData() {
@@ -61,6 +72,98 @@ public class frmCabezas {
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void setupHeadListListener() {
+        // Agregar un listener al ListView para capturar los eventos de selección
+        lstHeads.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            // Obtener el índice seleccionado
+            int selectedIndex = lstHeads.getSelectionModel().getSelectedIndex();
+
+            if (selectedIndex >= 0) {
+                // Obtener el objeto grhData correspondiente al índice seleccionado
+                headData selectedHead = headList.get(selectedIndex);
+                updateEditor(selectedHead);
+
+                for (int i = 0; i <= 3; i++) {
+                    drawHeads(selectedHead, i);
+                }
+            }
+        });
+    }
+
+    private void updateEditor(headData selectedHead) {
+        // Obtenemos todos los datos
+        short Texture = selectedHead.getTexture();
+        short StartX = selectedHead.getStartX();
+        short StartY = selectedHead.getStartY();
+
+        txtNGrafico.setText(String.valueOf(Texture));
+        txtStartX.setText(String.valueOf(StartX));
+        txtStartY.setText(String.valueOf(StartY));
+
+    }
+
+    private void drawHeads(headData selectedHead, int heading) {
+        // Construir la ruta completa de la imagen para imagePath
+        String imagePath = configManager.getGraphicsDir() + selectedHead.getTexture() + ".png";
+        File imageFile = new File(imagePath);
+
+        // ¿La imagen existe?
+        if (imageFile.exists()) {
+            Image staticImage = new Image(imageFile.toURI().toString());
+
+            int textureX2 = 27;
+            int textureY2 = 32;
+            int textureX1 = selectedHead.getStartX();
+            int textureY1 = (heading * textureY2) + selectedHead.getStartY();
+
+            // Verificar que las coordenadas de recorte estén dentro de los límites de la imagen
+            if (textureX1 + textureX2 > staticImage.getWidth()) {
+                textureX1 = (int) staticImage.getWidth() - textureX2;
+            }
+            if (textureY1 + textureY2 > staticImage.getHeight()) {
+                textureY1 = (int) staticImage.getHeight() - textureY2;
+            }
+
+            // Recortar la región adecuada de la imagen completa
+            PixelReader pixelReader = staticImage.getPixelReader();
+            WritableImage croppedImage = new WritableImage(pixelReader, textureX1, textureY1, textureX2, textureY2);
+
+            // Desactivar la preservación de la relación de aspecto
+            imgNorte.setPreserveRatio(false);
+
+            // Mostrar la región recortada en el ImageView correspondiente
+            switch (heading) {
+                case 0:
+                    imgSur.setImage(croppedImage);
+                    break;
+                case 1:
+                    imgNorte.setImage(croppedImage);
+                    break;
+                case 2:
+                    imgOeste.setImage(croppedImage);
+                    break;
+                case 3:
+                    imgEste.setImage(croppedImage);
+                    break;
+                default:
+                    // Dirección desconocida
+                    System.out.println("Dirección desconocida: " + heading);
+                    break;
+            }
+
+        } else {
+            // El archivo no existe, mostrar un mensaje de error o registrar un mensaje de advertencia
+            System.out.println("displayStaticImage: El archivo de imagen no existe: " + imagePath);
+
+            // Limpiar todos los ImageViews
+            imgSur.setImage(null);
+            imgNorte.setImage(null);
+            imgOeste.setImage(null);
+            imgEste.setImage(null);
         }
     }
 
